ee98b49719e03464f2d25bd8fbfe86e9b42f1e38
Monika Nanda <monika@MacBook-Pro.local>
2020-08-26 13:12:51 -0400

    created item.js file and added the scripts for CRUD

--- src/api/item.js
+++ src/api/item.js
@@ -1,15 +1,32 @@
 import apiUrl from '../apiConfig'
 import axios from 'axios'
 
-export const itemCreate = (items, user) => {
+export const itemCreate = (item, user) => {
   console.log('inside api-item')
   console.log(user.token)
+  console.log(item)
   return axios({
     headers: {
-      'Authorization': `Bearer token=${user.token}`
+      'Authorization': `Token token=${user.token}`
     },
     url: apiUrl + '/items',
     method: 'POST',
+    data: {
+      item: {
+        name: item.name,
+        quantity: item.quantity,
+        price: item.price
+      }
+    }
+  })
+}
+export const index = (items, user) => {
+  return axios({
+    headers: {
+      'Authorization': `Token token=${user.token}`
+    },
+    url: apiUrl + '/items',
+    method: 'GET',
     data: {
       items: {
         name: items.name,
@@ -19,7 +36,7 @@ export const itemCreate = (items, user) => {
     }
   })
 }
-export const index = (items, user) => {
+export const show = (id, items, user) => {
   return axios({
     headers: {
       'Authorization': `Bearer token=${user.token}`
@@ -35,3 +52,28 @@ export const index = (items, user) => {
     }
   })
 }
+// export const delete = (id, user) => {
+//   return axios({
+//     headers: {
+//       'Authorization': `Bearer token=${user.token}`
+//     },
+//     url: apiUrl + '/items',
+//     method: 'DELETE',
+//   })
+// }
+// export const update = (id,items, user) => {
+//   return axios({
+//     headers: {
+//       'Authorization': `Bearer token=${user.token}`
+//     },
+//     url: apiUrl + '/items',
+//     method: 'PATCH',
+//     data: {
+//       items: {
+//         name: items.name,
+//         quantity: items.quantity,
+//         price: items.price
+//       }
+//     }
+//   })
+// }
