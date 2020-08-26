90621b679b8f7db5ee79fcd8a7e108394ff505e8
fionabeatricewong <fionabeatricewong@gmail.com>
2020-08-26 11:13:27 -0400

    Added content and image to Home component

---
+++ src/components/routes/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Layout from '../Layout.js'
+import fridge from './fridge-home.png'
+
+const Home = () => (
+  <Layout>
+    <img src={fridge} alt="fridge" />
+  </Layout>
+)
+
+export default Home
