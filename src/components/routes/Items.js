39a891a13d7e0413af283d274b647d394cb5d5c5
Monika Nanda <monika@MacBook-Pro.local>
2020-08-26 16:47:30 -0400

    created a component items index and index is successful  Co-authored-by: Fiona Wong <https://github.com/fionabeatricewong> Co-authored-by: Eugene Damiani <https://github.com/Eugene-Damiani>

--- src/components/routes/Items.js
+++ src/components/routes/Items.js
@@ -1,1 +1,57 @@
+import React, { Component } from 'react'
+import { Link, withRouter } from 'react-router-dom'
+import { index } from '../../api/item'
+import messages from '../AutoDismissAlert/messages'
+import Layout from '../../components/Layout'
+class Items extends Component {
+  constructor () {
+    super()
+    this.state = {
+      items: []
+    }
+  }
+  // handleChange = event => setState({
+  //   [event.target.name]: event.target.value
+  // })
+  componentDidMount () {
+    console.log(this.props)
+    const { user, msgAlert } = this.props
+    index(this.state, user)
+      .then(res => this.setState({ items: res.data.items }))
+      .then(() => msgAlert({
+        heading: 'Item Show Success',
+        message: messages.itemShowSuccess,
+        variant: 'success'
+      }))
+      .then(() => history.push('/'))
+      .catch(error => {
+        msgAlert({
+          heading: 'Item index Failed with error: ' + error.message,
+          message: messages.ItemIndexFailure,
+          variant: 'danger'
+        })
+      })
+  }
 
+  render () {
+  // const { name, quantity, price } = this.state
+    console.log(this.state, 'We are in render')
+    const items = this.state.items.map(item => (
+      <li key={item._id}>
+        <Link to={`/items/${item._id}`}>
+          {item.name}
+        </Link>
+      </li>
+    ))
+    return (
+      <Layout>
+        <h4>Items</h4>
+        <ul>
+          {items}
+        </ul>
+      </Layout>
+    )
+  }
+}
+
+export default withRouter(Items)
