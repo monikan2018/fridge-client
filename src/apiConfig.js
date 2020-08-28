5f7d3a65b5ab7c9c099565977cef26d52649fd4c
fionabeatricewong <fionabeatricewong@gmail.com>
2020-08-28 15:31:53 -0400

    Updated production url with app's heroku

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://stark-falls-98169.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
